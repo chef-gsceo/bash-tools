528eda8308fe7e62d1c55fd8ff691d9bf3edf3a0
chef <moltern.light@gmail.com>
2026-05-07 02:26:08 +0300

    change working area and colors

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -6,50 +6,58 @@ C_LOAD="\e[1;37m"
 # cpu temperature
 C_CPU="\e[1;32m"
 # disk temperature
-C_NVME="\e[1;31m"
+C_NVME="\e[1;36m"
 # gpu temperature
 C_GPU="\e[1;95m"
 # color reset
 C_RESET="\e[0m"
 
+# another terminal
+printf '\e[?1049h'
+# restore terminal on exit
+trap 'printf "\e[?1049l"' EXIT
 
 while true; do
-    clear
+
+    #cursor control
+    tput cup 0 0
+    tput ed 
 
     # load average parsing
     LOAD=$(awk '{print $1, $2, $3}' /proc/loadavg)
-    echo -e "${C_LOAD}load: ${LOAD}${C_RESET}"
+    printf "${C_LOAD}load: ${LOAD}${C_RESET}\n"
 
     # sensors parsing
     sensors | sed 's/ *(.*)//' | grep -v 'Adapter' | sed '/^$/d' | while read -r line; do
 
         case "$line" in
             k10temp-*)
-                echo -e "${C_CPU}${line}${C_RESET}"
+                printf "${C_CPU}${line}${C_RESET}\n"
                 ;;
             nvme-*)
-                echo -e "${C_NVME}${line}${C_RESET}"
+                printf "${C_NVME}${line}${C_RESET}\n"
                 ;;
             amdgpu-*)
-                echo -e "${C_GPU}${line}${C_RESET}"
+                printf "${C_GPU}${line}${C_RESET}\n"
                 ;;
 
             Tctl:*)
-                echo -e "${C_CPU}${line}${C_RESET}"
+                printf "${C_CPU}${line}${C_RESET}\n"
                 ;;
             Composite:*)
-                echo -e "${C_NVME}${line}${C_RESET}"
+                printf "${C_NVME}${line}${C_RESET}\n"
                 ;;
             edge:*|junction:*|mem:*|PPT:*|fan1:*|vddgfx:*)
-                echo -e "${C_GPU}${line}${C_RESET}"
+                printf "${C_GPU}${line}${C_RESET}\n"
                 ;;
 
             *)
-                echo "$line"
+                printf "${line}\n"
                 ;;
         esac
 
     done
-
+    
     sleep 1
+
 done
